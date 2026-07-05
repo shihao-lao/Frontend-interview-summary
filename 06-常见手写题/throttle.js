e91c5526ae1be1294129d5345b4f0c5e4ed59c7e
shihao-lao <1765030326@qq.com>
2026-07-05 21:49:08 +0800

    feat:防抖节流

---
+++ 06-常见手写题/throttle.js
@@ -0,0 +1,12 @@
+function throttle(fn, delay) {
+  let timer = null;
+  return function (...args) {
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+      timer = null;
+    }, delay);
+  };
+}
